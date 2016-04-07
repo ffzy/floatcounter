e3442cc872e8658f1eb84cc43b05fc457f0ef0ec
Yue Zhang <yue.zhang2@emc.com>
2016-04-07 02:19:35 +0000

    Make the locating jar file logic more reliable in the run_float_counter.sh script.

--- script/run_float_counter.sh
+++ script/run_float_counter.sh
@@ -13,6 +13,27 @@ Usage: $(basename $0) <input file> [rebuild]
 EOF
 }
 
+function build {
+    echo -n "Building... "
+    echo > $mvn_log
+    cd $fp_home
+    mvn package > $mvn_log || {
+        echo "Build failed. Please check $mvn_log for the problem."
+        cd -
+        return 1
+    }
+    cd -
+    echo "Done."
+}
+
+function run {
+    echo "Running... "
+    java -jar -DfileName="$file" $jar_file
+    typeset rc=$?
+    echo "Done."
+    return $rc
+}
+
 [[ -z $file ]] && {
     usage
     exit 1
@@ -20,32 +41,31 @@ EOF
 
 dirname=$(dirname $0)
 fp_home=${dirname}/..
+mvn_log=/tmp/mvn_build.log
 
-ls $fp_home/target/*-jar-with-dependencies.jar > /dev/null 2>&1 || rebuild="rebuild"
+ls $fp_home/target/floatcounter-*-jar-with-dependencies.jar > /dev/null 2>&1 \
+    || rebuild="rebuild"
 
 if [[ $rebuild == "rebuild" ]]; then
-    echo -n "Building... "
-    mvn_log=/tmp/mvn_build.log
-    echo > $mvn_log
-    cd $fp_home
-    mvn package > $mvn_log
-    cd -
-    echo "Done."
+    build || exit 1
 
     build_info=$(grep Building $mvn_log | head -1) 
-    jar_file_name="$(echo $build_info | cut -d: -f2)-$(echo $build_info | cut -d: -f4)-jar-with-dependencies.jar"
+    jar_file_name="$(echo $build_info | cut -d: -f2)-$(echo $build_info \
+        | cut -d: -f4)-jar-with-dependencies.jar"
     jar_file=$fp_home/target/$jar_file_name
+    echo "Jar file: $jar_file"
 else
-    jar_file=$(ls $fp_home/target/*-jar-with-dependencies.jar)
+    jar_files=$(ls $fp_home/target/floatcounter-*-jar-with-dependencies.jar)
+    if [[ -z $jar_files ]]; then
+        echo "Error: No jar file found." \
+            "Please enable the \"rebuild\" option and run again."
+        exit 1
+    fi
+
+    echo "Jar file(s) found: $jar_files."
+    jar_file=$(echo "$jar_files" | tail -1)
+    echo "Use the latest version - $jar_file)"
 fi
 
-echo "Jar file: $jar_file"
-
-ls $jar_file > /dev/null || {
-    echo "Error: No jar file found."
-    exit 1
-}
-
-echo "Running... "
-java -jar -DfileName="$file" $jar_file
-echo "Done."
+run
+exit $?
